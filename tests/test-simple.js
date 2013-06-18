eb8a1702566cb8fb50c0c3adf0ddfca6480eed5d
Patrick Dillon <dillon76@gmail.com>
2013-06-18 19:49:28 -0400

    added formatters, color coding console logging... yaml formatting for console

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -5,37 +5,49 @@ var dribble = require('../index.js'),
     config = require('./dribble-config.json'),
     assert = require('assert');
 
-try
-{
-dribble.config(config);
+try {
+    dribble.config(config);
 
-dribble.info('I am gonna test grouping');
+//
+// simple console alias you can disable
+//
+
+    dribble.console('Begin testing, be sure to look at ./dribble-config.json for settings!', __filename);
 
 
 //
 // logging some info
 //
-dribble.info('Logging some info');
+    dribble.info('Logging some request info',
+        {url:'/users/address', token:'1223456',
+            body:{street:'2100 W Division', city:'Chicago'},
+            units: [{x:1},2]
+        });
 
 
 //
 // log a warning
 //
-dribble.warning('This is a warning');
+    dribble.warning('This is a warning');
+
+//
+// log a milestone in the app
+//
+    dribble.milestone('We reached a milestone');
 
 //
 // log an error
 //
-var err = new Error();
-dribble.error('New error!',err);
+    var err = new Error('test');
+    dribble.error('New error!', err);
 
 
 //
 // do console logging you can disable
 //
 
-dribble.console(process.version,'my node version',__dirname,'this path');
-dribble.console('Simple test passed.');
+
+    dribble.console('Simple test passed.');
 }
 catch (e) {
     assert.ifError(e);
